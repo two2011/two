425c9e21a1f7820f26951ba5f0dec959f648d825
Jakub Krawetkowski <krawetko+two@gmail.com>
2011-10-24 21:36:15 +0200

    Server Communication Implemented

--- mud/server/src/main/java/pl/edu/agh/two/mud/server/Server.java
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/Server.java
@@ -1,14 +1,29 @@
 package pl.edu.agh.two.mud.server;
 
-import pl.edu.agh.two.mud.common.*;
-import pl.edu.agh.two.mud.server.configuration.*;
+import java.io.*;
+import java.net.*;
 
 public class Server {
 
-	SomeRMIInterface someRMIInterface;
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(3306);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 
-	public static void main(String[] args) {
-		Server server = (Server) ApplicationContext.getBean("server");
-		System.out.println(server);
+		try {
+			while (true) {
+				try {
+					new Service(serverSocket.accept()).start();
+				} catch (Exception e) {
+				}
+			}
+		} finally {
+			serverSocket.close();
+		}
+
 	}
 }
