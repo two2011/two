5019e468b5c59512287dc049fb4c3019aa0789a5
Kamil Sobon <iso.poczta@gmail.com>
2011-11-06 12:04:29 +0100

    Dispatcher enriched of new method for dispatching command (not only parsed command).

--- mud/common/src/main/java/pl/edu/agh/two/mud/common/command/dispatcher/Dispatcher.java
+++ mud/common/src/main/java/pl/edu/agh/two/mud/common/command/dispatcher/Dispatcher.java
@@ -1,9 +1,12 @@
 package pl.edu.agh.two.mud.common.command.dispatcher;
 
-import pl.edu.agh.two.mud.common.command.*;
+import pl.edu.agh.two.mud.common.command.Command;
+import pl.edu.agh.two.mud.common.command.IParsedCommand;
 
 public interface Dispatcher {
-	
+
 	void dispatch(IParsedCommand parsedCommand);
+
+	void dispatch(Command command);
 
 }
--- mud/common/src/main/java/pl/edu/agh/two/mud/common/command/dispatcher/SynchronizedDispatcher.java
+++ mud/common/src/main/java/pl/edu/agh/two/mud/common/command/dispatcher/SynchronizedDispatcher.java
@@ -1,30 +1,36 @@
 package pl.edu.agh.two.mud.common.command.dispatcher;
 
-import pl.edu.agh.two.mud.common.command.*;
-import pl.edu.agh.two.mud.common.command.executor.*;
-import pl.edu.agh.two.mud.common.command.factory.*;
-import pl.edu.agh.two.mud.common.command.provider.*;
+import pl.edu.agh.two.mud.common.command.Command;
+import pl.edu.agh.two.mud.common.command.IParsedCommand;
+import pl.edu.agh.two.mud.common.command.executor.CommandExecutor;
+import pl.edu.agh.two.mud.common.command.factory.CommandFactory;
+import pl.edu.agh.two.mud.common.command.provider.CommandExecutorProvider;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class SynchronizedDispatcher implements Dispatcher {
-	
+
 	private CommandFactory commandFactory;
 	private CommandExecutorProvider commandExecutorProvider;
 
 	@Override
 	public synchronized void dispatch(IParsedCommand parsedCommand) {
-		Command command = commandFactory.create(parsedCommand);
-		CommandExecutor commandExecutor = commandExecutorProvider.getExecutorForCommand(command);
+		dispatch(commandFactory.create(parsedCommand));
+	}
+
+	@Override
+	public void dispatch(Command command) {
+		CommandExecutor commandExecutor = commandExecutorProvider
+				.getExecutorForCommand(command);
 		commandExecutor.execute(command);
 	}
-	
-	public void setCommandExecutorProvider(CommandExecutorProvider commandExecutorProvider) {
+
+	public void setCommandExecutorProvider(
+			CommandExecutorProvider commandExecutorProvider) {
 		this.commandExecutorProvider = commandExecutorProvider;
 	}
-	
+
 	public void setCommandFactory(CommandFactory commandFactory) {
 		this.commandFactory = commandFactory;
 	}
-	
 
 }
