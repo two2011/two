cf39276d4e19066c5c1d28ab62588f0faeea4230
Marcin Kuras <kpot88@gmail.com>
2011-11-10 19:18:52 +0100

    #48 First implementation of whispering.

---
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/command/WhisperCommand.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.two.mud.server.command;
+
+import pl.edu.agh.two.mud.common.command.UICommand;
+import pl.edu.agh.two.mud.common.command.annotation.Alias;
+import pl.edu.agh.two.mud.common.command.annotation.OrderedParam;
+import pl.edu.agh.two.mud.common.command.type.Text;
+
+@Alias({ "szepcz", "sz" })
+public class WhisperCommand extends UICommand {
+
+	@OrderedParam(1)
+	private String target;
+
+	@OrderedParam(2)
+	private Text content;
+
+	@Override
+	public String getDescription() {
+		return "Komenda szeptania - przesya wiadomosc do wybranego gracza bedacego na tym samym polu\n"
+				+ "\t szepcz[sz] <user> <tresc>";
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public Text getContent() {
+		return content;
+	}
+
+}
---
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/executor/WhisperCommandExecutor.java
@@ -0,0 +1,50 @@
+package pl.edu.agh.two.mud.server.executor;
+
+import java.io.IOException;
+
+import pl.edu.agh.two.mud.common.IPlayer;
+import pl.edu.agh.two.mud.common.command.executor.CommandExecutor;
+import pl.edu.agh.two.mud.common.command.type.Text;
+import pl.edu.agh.two.mud.server.IServiceRegistry;
+import pl.edu.agh.two.mud.server.Service;
+import pl.edu.agh.two.mud.server.command.WhisperCommand;
+import pl.edu.agh.two.mud.server.world.exception.NoPlayerWithSuchNameException;
+import pl.edu.agh.two.mud.server.world.model.Board;
+
+public class WhisperCommandExecutor implements CommandExecutor<WhisperCommand> {
+
+	private Board board;
+	private IServiceRegistry serviceRegistry;
+
+	@Override
+	public void execute(WhisperCommand command) {
+		Service currentService = serviceRegistry.getCurrentService();
+		IPlayer currentPlayer = serviceRegistry.getPlayer(currentService);
+		Text content = command.getContent();
+		try {
+			IPlayer targetPlayer = board.getPlayerByName(command.getTarget());
+			try {
+				serviceRegistry.getService(targetPlayer).writeObject(
+						currentPlayer.getName() + " szepcze: "
+								+ content.getText());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (NoPlayerWithSuchNameException e) {
+			try {
+				currentService.writeObject("Nie ma takiego gracza na tym polu");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public void setServiceRegistry(IServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
+	}
+
+}
