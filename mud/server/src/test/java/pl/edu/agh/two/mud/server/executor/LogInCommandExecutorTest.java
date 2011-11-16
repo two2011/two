ba68acbd5a4a411aa9da735ad5fc6acb41c6d157
Marcin Kuras <kpot88@gmail.com>
2011-11-16 16:42:49 +0100

    NT Test fixed.

--- mud/server/src/test/java/pl/edu/agh/two/mud/server/executor/LogInCommandExecutorTest.java
+++ mud/server/src/test/java/pl/edu/agh/two/mud/server/executor/LogInCommandExecutorTest.java
@@ -1,86 +1,91 @@
 package pl.edu.agh.two.mud.server.executor;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import pl.edu.agh.two.mud.common.Player;
 import pl.edu.agh.two.mud.server.Service;
 import pl.edu.agh.two.mud.server.ServiceRegistry;
 import pl.edu.agh.two.mud.server.command.LogInCommand;
 import pl.edu.agh.two.mud.server.world.exception.NoPlayerWithSuchNameException;
 import pl.edu.agh.two.mud.server.world.model.Board;
-
-import java.io.IOException;
-
-import static org.mockito.Mockito.*;
-
+import pl.edu.agh.two.mud.server.world.model.SampleBoard;
 
 public class LogInCommandExecutorTest {
-    LogInCommandExecutor executor = new LogInCommandExecutor();
-    Board board;
-    ServiceRegistry serviceRegistry = mock(ServiceRegistry.class);
-    Service service = mock(Service.class);
+	LogInCommandExecutor executor = new LogInCommandExecutor();
+	Board board;
+	ServiceRegistry serviceRegistry = mock(ServiceRegistry.class);
+	Service service = mock(Service.class);
 
-    @Before
-    public void before() {
-        board = new Board();
-        when(serviceRegistry.getCurrentService()).thenReturn(service);
-        executor.setBoard(board);
-        executor.setServiceRegistry(serviceRegistry);
-    }
+	@Before
+	public void before() {
+		board = new SampleBoard();
+		when(serviceRegistry.getCurrentService()).thenReturn(service);
+		executor.setBoard(board);
+		executor.setServiceRegistry(serviceRegistry);
+	}
 
-    @Test
-    public void shouldSuccessfullyExecuteLogInCommand() throws NoPlayerWithSuchNameException {
-        // given
-        LogInCommand command = mockCommand();
+	@Test
+	public void shouldSuccessfullyExecuteLogInCommand()
+			throws NoPlayerWithSuchNameException {
+		// given
+		LogInCommand command = mockCommand();
 
-        Player player = createPlayer("krzyho", "correctPassword");
-        board.addPlayer(player);
+		Player player = createPlayer("krzyho", "correctPassword");
+		board.addPlayer(player);
 
-        // when
-        executor.execute(command);
+		// when
+		executor.execute(command);
 
-        // then
-        verify(serviceRegistry).bindPlayerToService(service, player);
-    }
+		// then
+		verify(serviceRegistry).bindPlayerToService(service, player);
+	}
 
-    @Test
-    public void shouldNotLoginWithWrongPassword() throws NoPlayerWithSuchNameException, IOException {
-        // given
-        LogInCommand command = mockCommand();
+	@Test
+	public void shouldNotLoginWithWrongPassword()
+			throws NoPlayerWithSuchNameException, IOException {
+		// given
+		LogInCommand command = mockCommand();
 
-        Player player = createPlayer("krzyho", "wrongPassword");
-        board.addPlayer(player);
+		Player player = createPlayer("krzyho", "wrongPassword");
+		board.addPlayer(player);
 
-        // when
-        executor.execute(command);
+		// when
+		executor.execute(command);
 
-        // then
-        verify(service).writeObject("Zle haslo");
-    }
+		// then
+		verify(service).writeObject("Zle haslo");
+	}
 
-    @Test
-    public void shouldNotLoginWhenNoPlayerRegistered() throws IOException {
-        // given
-        LogInCommand command = mockCommand();
+	@Test
+	public void shouldNotLoginWhenNoPlayerRegistered() throws IOException {
+		// given
+		LogInCommand command = mockCommand();
 
-        // when
-        executor.execute(command);
+		// when
+		executor.execute(command);
 
-        // then
-        verify(service).writeObject("Nie ma takiego gracza");
-    }
+		// then
+		verify(service).writeObject("Nie ma takiego gracza");
+	}
 
-    private LogInCommand mockCommand() {
-        LogInCommand command = mock(LogInCommand.class);
-        when(command.getLogin()).thenReturn("krzyho");
-        when(command.getPassword()).thenReturn("correctPassword");
-        return command;
-    }
+	private LogInCommand mockCommand() {
+		LogInCommand command = mock(LogInCommand.class);
+		when(command.getLogin()).thenReturn("krzyho");
+		when(command.getPassword()).thenReturn("correctPassword");
+		return command;
+	}
 
-    private Player createPlayer(String name, String password) {
-        Player player = new Player();
-        player.setName(name);
-        player.setPassword(password);
-        return player;
-    }
+	private Player createPlayer(String name, String password) {
+		Player player = new Player();
+		player.setName(name);
+		player.setPassword(password);
+		return player;
+	}
 }
