ac67147c925f810a0b7fb768ff57ff003481009f
two <two@mail.com>
2011-11-24 19:46:30 +0100

    Fight logic interface.

---
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/world/fight/Fight.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.two.mud.server.world.fight;
+
+import pl.edu.agh.two.mud.common.IPlayer;
+
+public interface Fight {
+	
+	public void startFight(IPlayer playerOne, IPlayer playetTwo);
+	
+
+}
