7aa71d5c7d89b6f6d625c93a214c40c996b38087
Kamil Sobon <iso.poczta@gmail.com>
2011-11-24 20:07:02 +0100

    Command for refreshing implemented.

---
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/command/RefreshUICommand.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.two.mud.server.command;
+
+import pl.edu.agh.two.mud.common.command.UICommand;
+import pl.edu.agh.two.mud.common.command.annotation.Alias;
+
+@Alias({"odswiez", "o"})
+public class RefreshUICommand extends UICommand {
+
+	@Override
+	public String getDescription() {
+		return "Odswieza zawartosc";
+	}
+
+}
