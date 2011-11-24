4f906fd4f533deebd511cdd9e0bf8531e0652f18
two <two@mail.com>
2011-11-24 19:41:06 +0100

    RunCommand added.

---
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/command/RunCommand.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.two.mud.server.command;
+
+import pl.edu.agh.two.mud.common.command.UICommand;
+import pl.edu.agh.two.mud.common.command.annotation.Alias;
+
+@Alias({"run"})
+public class RunCommand extends UICommand {
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
