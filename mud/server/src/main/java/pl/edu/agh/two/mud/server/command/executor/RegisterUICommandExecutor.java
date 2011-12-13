7d491c5bdf9835968ad517658a8db10385afd720
Marcin Kuras <kpot88@gmail.com>
2011-12-13 17:01:33 +0100

    NT Fixes in register command.

--- mud/server/src/main/java/pl/edu/agh/two/mud/server/command/executor/RegisterUICommandExecutor.java
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/command/executor/RegisterUICommandExecutor.java
@@ -5,30 +5,52 @@ import java.util.Random;
 import javax.mail.MessagingException;
 
 import pl.edu.agh.two.mud.common.Player;
+import pl.edu.agh.two.mud.common.command.dispatcher.Dispatcher;
 import pl.edu.agh.two.mud.common.command.executor.CommandExecutor;
+import pl.edu.agh.two.mud.common.message.MessageType;
 import pl.edu.agh.two.mud.server.command.RegisterUICommand;
+import pl.edu.agh.two.mud.server.command.SendMessageToUserCommand;
+import pl.edu.agh.two.mud.server.command.exception.ClientAwareException;
 import pl.edu.agh.two.mud.server.mail.Mailer;
+import pl.edu.agh.two.mud.server.world.exception.NoPlayerWithSuchNameException;
 import pl.edu.agh.two.mud.server.world.model.Board;
 
 public class RegisterUICommandExecutor implements
 		CommandExecutor<RegisterUICommand> {
 
+	private static final String MESSAGE_ON_WRONG_ADDRESS = "Bledny adres e-mail";
+
+	private static final String MESSAGE_ON_EXISTING_LOGIN = "Podany login juz istnieje";
+
+	private static final String MESSAGE_ON_SUCCESS = "Konto zostalo utworzone. Dane dostepowe zostaly wyslane na podany adres.";
+
 	private Board board;
+
 	private Mailer mailer;
 
+	private Dispatcher dispatcher;
+
 	@Override
-	public void execute(RegisterUICommand command) {
-		Player player = new Player();
+	public void execute(RegisterUICommand command) throws ClientAwareException {
 		try {
-			String password = generatePassword();
-			mailer.sendRegistrationMail(command.getEmail(), command.getLogin(),
-					password);
-			player.setName(command.getLogin());
-			player.setPassword(password);
-			board.addPlayer(player);
-		} catch (MessagingException e) {
-			e.printStackTrace();
+			board.getPlayerByName(command.getLogin());
+			throw new ClientAwareException(MESSAGE_ON_EXISTING_LOGIN);
+		} catch (NoPlayerWithSuchNameException e) {
+			try {
+				Player player = new Player();
+				String password = generatePassword();
+				mailer.sendRegistrationMail(command.getEmail(),
+						command.getLogin(), password);
+				player.setName(command.getLogin());
+				player.setPassword(password);
+				board.addPlayer(player);
+				dispatcher.dispatch(new SendMessageToUserCommand(
+						MESSAGE_ON_SUCCESS, MessageType.INFO));
+			} catch (MessagingException e1) {
+				throw new ClientAwareException(MESSAGE_ON_WRONG_ADDRESS);
+			}
 		}
+
 	}
 
 	private String generatePassword() {
@@ -44,5 +66,13 @@ public class RegisterUICommandExecutor implements
 
 	public void setMailer(Mailer mailer) {
 		this.mailer = mailer;
+	}
+
+	public Dispatcher getDispatcher() {
+		return dispatcher;
+	}
+
+	public void setDispatcher(Dispatcher dispatcher) {
+		this.dispatcher = dispatcher;
 	}
 }
