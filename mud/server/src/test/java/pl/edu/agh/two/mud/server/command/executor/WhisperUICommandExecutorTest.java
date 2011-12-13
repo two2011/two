7371ca657deb80ca627947a4be9758153386dc6c
Kamil Sobon <iso.poczta@gmail.com>
2011-12-13 20:47:56 +0100

    [NT] Merge bug fix.

--- mud/server/src/test/java/pl/edu/agh/two/mud/server/command/executor/WhisperUICommandExecutorTest.java
+++ mud/server/src/test/java/pl/edu/agh/two/mud/server/command/executor/WhisperUICommandExecutorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
@@ -35,7 +36,6 @@ public class WhisperUICommandExecutorTest {
 	private Service currentService;
 	private Dispatcher dispatcher;
 	private WhisperUICommand command;
-	private Dispatcher dispatcher;
 
 	@Before
 	public void prepareTest() {
