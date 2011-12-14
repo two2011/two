766d1415cc96a3e3029ca4e1fd0a3fec1574f780
Kamil Sobon <iso.poczta@gmail.com>
2011-12-14 21:17:02 +0100

    [NT] RefreshUICommandExecutor with test fixed.

--- mud/server/src/main/java/pl/edu/agh/two/mud/server/command/executor/RefreshUICommandExecutor.java
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/command/executor/RefreshUICommandExecutor.java
@@ -1,37 +1,29 @@
 package pl.edu.agh.two.mud.server.command.executor;
 
-import java.io.IOException;
-
 import pl.edu.agh.two.mud.common.IPlayer;
+import pl.edu.agh.two.mud.common.command.dispatcher.Dispatcher;
 import pl.edu.agh.two.mud.common.command.exception.CommandExecutingException;
-import pl.edu.agh.two.mud.common.command.exception.FatalException;
 import pl.edu.agh.two.mud.common.command.executor.CommandExecutor;
+import pl.edu.agh.two.mud.common.message.MessageType;
 import pl.edu.agh.two.mud.server.IServiceRegistry;
 import pl.edu.agh.two.mud.server.Service;
 import pl.edu.agh.two.mud.server.command.RefreshUICommand;
+import pl.edu.agh.two.mud.server.command.SendMessageToUserCommand;
 import pl.edu.agh.two.mud.server.world.model.Board;
 
-public class RefreshUICommandExecutor implements
-		CommandExecutor<RefreshUICommand> {
+public class RefreshUICommandExecutor implements CommandExecutor<RefreshUICommand> {
 
 	private IServiceRegistry serviceRegistry;
-
 	private Board board;
+	private Dispatcher dispatcher;
 
 	@Override
-	public void execute(RefreshUICommand command)
-			throws CommandExecutingException {
-
+	public void execute(RefreshUICommand command) throws CommandExecutingException {
 		Service service = serviceRegistry.getCurrentService();
 		IPlayer player = serviceRegistry.getPlayer(service);
 
-		try {
-			service.writeObject(board.getPlayersPosition(player)
-					.getFormattedFieldSummary());
-		} catch (IOException e) {
-			throw new FatalException(e);
-		}
-
+		dispatcher.dispatch(new SendMessageToUserCommand(board.getPlayersPosition(player).getFormattedFieldSummary(),
+				MessageType.INFO));
 	}
 
 	public void setServiceRegistry(IServiceRegistry serviceRegistry) {
@@ -40,6 +32,10 @@ public class RefreshUICommandExecutor implements
 
 	public void setBoard(Board board) {
 		this.board = board;
+	}
+
+	public void setDispatcher(Dispatcher dispatcher) {
+		this.dispatcher = dispatcher;
 	}
 
 }
--- mud/server/src/test/java/pl/edu/agh/two/mud/server/command/executor/RefreshUICommandExecutorTest.java
+++ mud/server/src/test/java/pl/edu/agh/two/mud/server/command/executor/RefreshUICommandExecutorTest.java
@@ -1,23 +1,21 @@
 package pl.edu.agh.two.mud.server.command.executor;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import pl.edu.agh.two.mud.common.IPlayer;
+import pl.edu.agh.two.mud.common.command.dispatcher.Dispatcher;
 import pl.edu.agh.two.mud.common.command.exception.CommandExecutingException;
+import pl.edu.agh.two.mud.common.message.MessageType;
 import pl.edu.agh.two.mud.server.IServiceRegistry;
 import pl.edu.agh.two.mud.server.Service;
 import pl.edu.agh.two.mud.server.command.RefreshUICommand;
+import pl.edu.agh.two.mud.server.command.SendMessageToUserCommand;
 import pl.edu.agh.two.mud.server.world.model.Board;
 import pl.edu.agh.two.mud.server.world.model.Field;
 
@@ -30,6 +28,7 @@ public class RefreshUICommandExecutorTest {
 	private Board board;
 	private Field field;
 	private Service currentService;
+	private Dispatcher dispatcher;
 
 	@Before
 	public void prepareTest() {
@@ -38,11 +37,13 @@ public class RefreshUICommandExecutorTest {
 		board = mock(Board.class);
 		field = mock(Field.class);
 		currentService = mock(Service.class);
+		dispatcher = mock(Dispatcher.class);
 		IPlayer player = mock(IPlayer.class);
 
 		executor = new RefreshUICommandExecutor();
 		executor.setBoard(board);
 		executor.setServiceRegistry(serviceRegistry);
+		executor.setDispatcher(dispatcher);
 
 		when(serviceRegistry.getCurrentService()).thenReturn(currentService);
 		when(serviceRegistry.getPlayer(currentService)).thenReturn(player);
@@ -51,31 +52,12 @@ public class RefreshUICommandExecutorTest {
 	}
 
 	@Test
-	public void sendingIOError() {
-		Throwable throwable = new IOException();
-		try {
-			doThrow(throwable).when(currentService).writeObject(any());
-		} catch (IOException e) {
-		}
-
-		try {
-			executor.execute(command);
-			fail("Exception expected");
-		} catch (CommandExecutingException e) {
-			assertEquals(e.getCause(), throwable);
-		}
-	}
-
-	@Test
 	public void refreshCommand() {
 
 		try {
 			executor.execute(command);
-
-			try {
-				verify(currentService).writeObject(SUMMARY);
-			} catch (IOException e) {
-			}
+			verify(dispatcher).dispatch(
+					new SendMessageToUserCommand(field.getFormattedFieldSummary(), MessageType.INFO));
 		} catch (CommandExecutingException e) {
 			fail("Exception unexpected");
 		}
