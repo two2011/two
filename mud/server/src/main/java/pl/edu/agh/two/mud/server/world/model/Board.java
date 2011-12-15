d70c0c1bf3ea9640f6c168245039140c485672bb
Kamil Sobon <iso.poczta@gmail.com>
2011-12-15 16:31:09 +0100

    [NT] Mistake commit reverted.

--- mud/server/src/main/java/pl/edu/agh/two/mud/server/world/model/Board.java
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/world/model/Board.java
@@ -1,124 +1,114 @@
 package pl.edu.agh.two.mud.server.world.model;
 
-import pl.edu.agh.two.mud.common.IPlayer;
-import pl.edu.agh.two.mud.common.Player;
-import pl.edu.agh.two.mud.server.world.exception.NoPlayerWithSuchNameException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import java.util.*;
+import pl.edu.agh.two.mud.common.IPlayer;
+import pl.edu.agh.two.mud.server.world.exception.NoPlayerWithSuchNameException;
 
 public class Board {
 
-    private String name;
-    private String description;
+	private String name;
+	private String description;
 
-    private Field[][] fields;
+	private Field[][] fields;
 
-    private Map<IPlayer, Field> playersOnFields = new HashMap<IPlayer, Field>();
-    private Field startingField;
-    private int xAxisSize;
-    private int yAxisSize;
-    
-    public Board() {
-    	Player iso = new Player();
-    	iso.setName("iso");
-    	iso.setPassword("iso");
-    	
-    	Player ktos = new Player();
-    	ktos.setName("ktos");
-    	ktos.setPassword("ktos");
-    	
-		addPlayer(iso);
-		addPlayer(ktos);
+	private Map<IPlayer, Field> playersOnFields = new HashMap<IPlayer, Field>();
+	private Field startingField;
+	private int xAxisSize;
+	private int yAxisSize;
+
+	public String getName() {
+		return name;
 	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public Field[][] getFields() {
+		return fields;
+	}
 
-    public Field[][] getFields() {
-        return fields;
-    }
+	public void setFields(Field[][] fields) {
+		this.fields = fields.clone();
+	}
 
-    public void setFields(Field[][] fields) {
-        this.fields = fields.clone();
-    }
+	public Field getStartingField() {
+		return startingField;
+	}
 
-    public Field getStartingField() {
-        return startingField;
-    }
+	public void setStartingField(Field startingField) {
+		this.startingField = startingField;
+	}
 
-    public void setStartingField(Field startingField) {
-        this.startingField = startingField;
-    }
+	public void setxAxisSize(int xAxisSize) {
+		this.xAxisSize = xAxisSize;
+	}
 
-    public void setxAxisSize(int xAxisSize) {
-        this.xAxisSize = xAxisSize;
-    }
+	public void setyAxisSize(int yAxisSize) {
+		this.yAxisSize = yAxisSize;
+	}
 
-    public void setyAxisSize(int yAxisSize) {
-        this.yAxisSize = yAxisSize;
-    }
+	public List<Direction> getPossibleDirections(Field field) {
+		List<Direction> result = new ArrayList<Direction>();
+		int x = field.getX();
+		int y = field.getY();
+		if (y > 0 && fields[y - 1][x] != null) {
+			result.add(Direction.N);
+		}
+		if (y < yAxisSize - 1 && fields[y + 1][x] != null) {
+			result.add(Direction.S);
+		}
+		if (x > 0 && fields[y][x - 1] != null) {
+			result.add(Direction.W);
+		}
+		if (x < xAxisSize - 1 && fields[y][x + 1] != null) {
+			result.add(Direction.E);
+		}
 
-    public List<Direction> getPossibleDirections(Field field) {
-        List<Direction> result = new ArrayList<Direction>();
-        int x = field.getX();
-        int y = field.getY();
-        if (y > 0 && fields[y - 1][x] != null) {
-            result.add(Direction.N);
-        }
-        if (y < yAxisSize - 1 && fields[y + 1][x] != null) {
-            result.add(Direction.S);
-        }
-        if (x > 0 && fields[y][x - 1] != null) {
-            result.add(Direction.W);
-        }
-        if (x < xAxisSize - 1 && fields[y][x + 1] != null) {
-            result.add(Direction.E);
-        }
+		return result;
+	}
 
-        return result;
-    }
+	public boolean addPlayer(IPlayer player) {
+		playersOnFields.put(player, startingField);
+		return playersOnFields.containsKey(player);
+	}
 
-    public boolean addPlayer(IPlayer player) {
-        playersOnFields.put(player, startingField);
-        return playersOnFields.containsKey(player);
-    }
+	public boolean removePlayer(IPlayer player) {
+		playersOnFields.put(player, null);
+		return playersOnFields.containsKey(player);
+	}
 
-    public boolean removePlayer(IPlayer player) {
-        playersOnFields.put(player, null);
-        return playersOnFields.containsKey(player);
-    }
+	public Set<IPlayer> getPlayers() {
+		return playersOnFields.keySet();
+	}
 
-    public Set<IPlayer> getPlayers() {
-        return playersOnFields.keySet();
-    }
+	public IPlayer getPlayerByName(String playerName) throws NoPlayerWithSuchNameException {
+		for (IPlayer player : playersOnFields.keySet()) {
+			if (player.getName().equals(playerName)) {
+				return player;
+			}
+		}
+		throw new NoPlayerWithSuchNameException(playerName);
+	}
 
-    public IPlayer getPlayerByName(String playerName) throws NoPlayerWithSuchNameException {
-        for (IPlayer player : playersOnFields.keySet()) {
-            if (player.getName().equals(playerName)) {
-                return player;
-            }
-        }
-        throw new NoPlayerWithSuchNameException(playerName);
-    }
+	public Field getPlayersPosition(IPlayer player) {
+		return playersOnFields.get(player);
+	}
 
-    public Field getPlayersPosition(IPlayer player) {
-        return playersOnFields.get(player);
-    }
-
-    public void setPlayersPosition(IPlayer player, Field field) {
-        playersOnFields.put(player, field);
-    }
+	public void setPlayersPosition(IPlayer player, Field field) {
+		playersOnFields.put(player, field);
+	}
 }
