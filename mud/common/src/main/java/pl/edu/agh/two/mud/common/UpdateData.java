3f7b22ae97274b60c29f02e892515945f600d63b
Marcin Kuras <kpot88@gmail.com>
2012-01-05 11:56:37 +0100

    Refreshing fixed. Advancing to the next level works.

--- mud/common/src/main/java/pl/edu/agh/two/mud/common/UpdateData.java
+++ mud/common/src/main/java/pl/edu/agh/two/mud/common/UpdateData.java
@@ -1,71 +1,81 @@
 package pl.edu.agh.two.mud.common;
 
-import java.io.*;
+import java.io.Serializable;
 
-public class UpdateData implements Serializable{
-	
-    private int strength;
+public class UpdateData implements Serializable {
 
-    private int power;
+	private static final long serialVersionUID = 7072973000316251332L;
 
-    private int agililty;
+	private String name;
 
-    private int gold;
+	private int strength;
 
-    private int experience ;
+	private int power;
 
-    private int level;
+	private int agililty;
 
-    private int healthPoints;
+	private int gold;
 
-    private int maxHealthPoints;
+	private int experience;
 
-    public int getStrength() {
-        return strength;
-    }
+	private int level;
 
-    public int getPower() {
-        return power;
-    }
+	private int healthPoints;
 
-    public int getAgililty() {
-        return agililty;
-    }
+	private int maxHealthPoints;
 
-    public int getGold() {
-        return gold;
-    }
+	public int getStrength() {
+		return strength;
+	}
 
-    public int getExperience() {
-        return experience;
-    }
+	public int getPower() {
+		return power;
+	}
 
-    public int getLevel() {
-        return level;
-    }
+	public int getAgililty() {
+		return agililty;
+	}
 
-    public int getHealthPoints() {
-        return healthPoints;
-    }
+	public int getGold() {
+		return gold;
+	}
 
-    public int getMaxHealthPoints() {
-        return maxHealthPoints;
-    }
+	public int getExperience() {
+		return experience;
+	}
 
-    public UpdateData(int strength, int power, int agililty, int gold, int experience, int level, int healthPoints,
-            int maxHealthPoints) {
-        super();
-        this.strength = strength;
-        this.power = power;
-        this.agililty = agililty;
-        this.gold = gold;
-        this.experience = experience;
-        this.level = level;
-        this.healthPoints = healthPoints;
-        this.maxHealthPoints = maxHealthPoints;
-    }
-    
-    
+	public int getLevel() {
+		return level;
+	}
 
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+
+	public int getMaxHealthPoints() {
+		return maxHealthPoints;
+	}
+
+	public UpdateData(String name, int strength, int power, int agililty,
+			int gold, int experience, int level, int healthPoints,
+			int maxHealthPoints) {
+		super();
+		this.strength = strength;
+		this.power = power;
+		this.agililty = agililty;
+		this.gold = gold;
+		this.experience = experience;
+		this.level = level;
+		this.healthPoints = healthPoints;
+		this.maxHealthPoints = maxHealthPoints;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
--- mud/server/src/main/java/pl/edu/agh/two/mud/server/world/fight/impl/PlayersFight.java
+++ mud/server/src/main/java/pl/edu/agh/two/mud/server/world/fight/impl/PlayersFight.java
@@ -31,27 +31,30 @@ public class PlayersFight implements Fight {
 		IPlayer playerOne = (IPlayer) creatureOne;
 		IPlayer playerTwo = (IPlayer) creatureTwo;
 
-		dispatcher.dispatch(new SendMessageToUserCommand(String.format("Zaatkowales gracza %s", playerOne.getName()),
-				MessageType.INFO));
-		dispatcher.dispatch(new SendMessageToUserCommand(playerTwo, String.format(
-				"Zostales zaatakowany przez gracza %s", playerOne.getName()), MessageType.INFO));
+		dispatcher
+				.dispatch(new SendMessageToUserCommand(String.format(
+						"Zaatkowales gracza %s", playerOne.getName()),
+						MessageType.INFO));
+		dispatcher.dispatch(new SendMessageToUserCommand(playerTwo, String
+				.format("Zostales zaatakowany przez gracza %s",
+						playerOne.getName()), MessageType.INFO));
 
 		int whoAttacksFirst = random.nextInt(2);
 		switch (whoAttacksFirst) {
-			case 0:
-				switchAttackingPlayer(creatureTwo, creatureOne);
-				break;
-			case 1:
-				switchAttackingPlayer(creatureOne, creatureTwo);
-				break;
+		case 0:
+			switchAttackingPlayer(creatureTwo, creatureOne);
+			break;
+		case 1:
+			switchAttackingPlayer(creatureOne, creatureTwo);
+			break;
 		}
 
 	}
 
 	@Override
 	public void hit(ICreature creatureWhoHits) throws FatalException {
-		IPlayer playerWhoHits = (IPlayer)creatureWhoHits;
-		
+		IPlayer playerWhoHits = (IPlayer) creatureWhoHits;
+
 		IPlayer enemy = playerWhoHits.getEnemy();
 		int damage = random.nextInt(4) + 1;
 		enemy.subtractHealthPoints(damage);
@@ -60,21 +63,27 @@ public class PlayersFight implements Fight {
 		try {
 			enemyService.writeObject(enemy.createUpdateData());
 
-			dispatcher.dispatch(new SendMessageToUserCommand(playerWhoHits, String.format(
-					"Zadales przeciwnikowi %d pkt obrazen.", damage), MessageType.INFO));
-			dispatcher.dispatch(new SendMessageToUserCommand(enemy, String.format("Zadano ci %d pkt obrazen.", damage),
+			dispatcher.dispatch(new SendMessageToUserCommand(playerWhoHits,
+					String.format("Zadales przeciwnikowi %d pkt obrazen.",
+							damage), MessageType.INFO));
+			dispatcher.dispatch(new SendMessageToUserCommand(enemy, String
+					.format("Zadano ci %d pkt obrazen.", damage),
 					MessageType.INFO));
 
 			if (enemy.isAlive()) {
-				dispatcher.dispatch(new SendMessageToUserCommand(enemy, "Krwawisz!", MessageType.INFO));
+				dispatcher.dispatch(new SendMessageToUserCommand(enemy,
+						"Krwawisz!", MessageType.INFO));
 
 				switchAttackingPlayer(playerWhoHits, enemy);
 			} else {
-                int expToAdd = enemy.getLevel() * 100;
-                playerWhoHits.addExperience(expToAdd);
-                dispatcher.dispatch(new SendMessageToUserCommand(playerWhoHits, String.format(
-                        "Wygrales! Zdobyles %d pkt doswiadczenia.", expToAdd), MessageType.INFO));
-				dispatcher.dispatch(new SendMessageToUserCommand(enemy, "Zginales!", MessageType.INFO));
+				int expToAdd = enemy.getLevel() * 1000;
+				playerWhoHits.addExperience(expToAdd);
+				dispatcher.dispatch(new SendMessageToUserCommand(playerWhoHits,
+						String.format(
+								"Wygrales! Zdobyles %d pkt doswiadczenia.",
+								expToAdd), MessageType.INFO));
+				dispatcher.dispatch(new SendMessageToUserCommand(enemy,
+						"Zginales!", MessageType.INFO));
 
 				endFight(playerWhoHits, enemy);
 			}
@@ -85,18 +94,22 @@ public class PlayersFight implements Fight {
 
 	@Override
 	public void runFromFight(ICreature currentCreature, Direction direction) {
-		IPlayer currentPlayer = (IPlayer)currentCreature;
+		IPlayer currentPlayer = (IPlayer) currentCreature;
 		IPlayer enemy = currentPlayer.getEnemy();
 		boolean canRun = random.nextInt(2) > 0.5;
 		if (!canRun) {
-			dispatcher.dispatch(new SendMessageToUserCommand("Nie udalo Ci sie uciec", MessageType.INFO));
-			dispatcher.dispatch(new SendMessageToUserCommand(enemy, "Przeciwnik probowal uciec lecz mu sie nie udalo",
+			dispatcher.dispatch(new SendMessageToUserCommand(
+					"Nie udalo Ci sie uciec", MessageType.INFO));
+			dispatcher.dispatch(new SendMessageToUserCommand(enemy,
+					"Przeciwnik probowal uciec lecz mu sie nie udalo",
 					MessageType.INFO));
 
 			switchAttackingPlayer(currentPlayer, enemy);
 		} else {
-			dispatcher.dispatch(new SendMessageToUserCommand("Udalo Ci sie uciec", MessageType.INFO));
-			dispatcher.dispatch(new SendMessageToUserCommand(enemy, "Przeciwnikowi udalo sie uciec", MessageType.INFO));
+			dispatcher.dispatch(new SendMessageToUserCommand(
+					"Udalo Ci sie uciec", MessageType.INFO));
+			dispatcher.dispatch(new SendMessageToUserCommand(enemy,
+					"Przeciwnikowi udalo sie uciec", MessageType.INFO));
 
 			dispatcher.dispatch(new MoveUICommand(direction));
 			endFight(currentPlayer, enemy);
@@ -104,14 +117,18 @@ public class PlayersFight implements Fight {
 	}
 
 	public void switchAttackingPlayer(ICreature from, ICreature to) {
-		IPlayer playerFrom = (IPlayer)from;
-		IPlayer playerTo = (IPlayer)to;
-		
-		dispatcher.dispatch(new SendMessageToUserCommand(playerFrom, "Tura przeciwnika", MessageType.INFO));
-		dispatcher.dispatch(new SendMessageToUserCommand(playerTo, "Twoja tura", MessageType.INFO));
+		IPlayer playerFrom = (IPlayer) from;
+		IPlayer playerTo = (IPlayer) to;
 
-		sendAvailableCommands(playerFrom, AvailableCommands.getInstance().getFightOpponentTurnCommands());
-		sendAvailableCommands(playerTo, AvailableCommands.getInstance().getFightYouTurnCommands());
+		dispatcher.dispatch(new SendMessageToUserCommand(playerFrom,
+				"Tura przeciwnika", MessageType.INFO));
+		dispatcher.dispatch(new SendMessageToUserCommand(playerTo,
+				"Twoja tura", MessageType.INFO));
+
+		sendAvailableCommands(playerFrom, AvailableCommands.getInstance()
+				.getFightOpponentTurnCommands());
+		sendAvailableCommands(playerTo, AvailableCommands.getInstance()
+				.getFightYouTurnCommands());
 	}
 
 	private void endFight(IPlayer player1, IPlayer player2) {
@@ -123,14 +140,23 @@ public class PlayersFight implements Fight {
 		player.setEnemy(null);
 
 		if (player.isAlive()) {
-			sendAvailableCommands(player, AvailableCommands.getInstance().getGameCommands());
+			sendAvailableCommands(player, AvailableCommands.getInstance()
+					.getGameCommands());
+			try {
+				serviceRegistry.getService(player).writeObject(player.createUpdateData());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		} else {
-			sendAvailableCommands(player, AvailableCommands.getInstance().getDeadPlayerCommands());
+			sendAvailableCommands(player, AvailableCommands.getInstance()
+					.getDeadPlayerCommands());
 		}
 	}
 
-	private void sendAvailableCommands(IPlayer player, Collection<UICommand> availableCommands) {
-		dispatcher.dispatch(new SendAvailableCommandsCommand(player, availableCommands));
+	private void sendAvailableCommands(IPlayer player,
+			Collection<UICommand> availableCommands) {
+		dispatcher.dispatch(new SendAvailableCommandsCommand(player,
+				availableCommands));
 	}
 
 	public void setServiceRegistry(IServiceRegistry serviceRegistry) {
